0185121e91512ba78d2411a2fcc9aafb25d5de40
Emily McMinn <emily@mcminn.ca>
2018-09-04 18:39:11 +0000

    added header partial and new URL form

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 
 
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -22,6 +24,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
